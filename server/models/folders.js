c0000aed7c85b318c61a3d5fb8eff17e91461e98
acebojohnrohan@gmail.com <acebojohnrohan@gmail.com>
2026-02-27 16:54:54 +0800

    Added folder schema

---
+++ server/models/folders.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Folder = mongoose.model('Folder', folderSchema);
+
+module.exports = Folder;
--- server/models/notes.js
+++ server/models/notes.js
@@ -4,6 +4,7 @@ const notesSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    folderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Folder' },
 }, { timestamps: true });
 
 const Note = mongoose.model('Note', notesSchema);
